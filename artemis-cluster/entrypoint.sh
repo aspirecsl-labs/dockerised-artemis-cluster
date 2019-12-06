cbdbe55d50f8e5a589799b4eec29c2351c5e9871
aspirecsl <labs@aspirecsl.com>
2019-12-06 12:22:16 +0000

    Bug Fixes
    - delays artemis instance creation to container start

--- artemis-cluster/entrypoint.sh
+++ artemis-cluster/entrypoint.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
 set -x
 
-export ARTEMIS_HOME=/artemis
-export PATH=$PATH:$ARTEMIS_HOME/bin:/var/artemis/"${CLUSTER_NAME}"/bin
+ARTEMIS_HOME=/artemis
+ARTEMIS_INSTANCE=/usr/artemis/instance
+export PATH=$PATH:$ARTEMIS_HOME/bin:$ARTEMIS_INSTANCE/bin
 
-BROKER_SED_EXPR="s/__HOST__/$(hostname -f)/g"
-BROKER=/var/artemis/${CLUSTER_NAME}/etc/broker.xml
+# only create an instance if one doesn't exist already
+if [ ! -d $ARTEMIS_INSTANCE ]
+then
+  echo "creating artemis instance..."
+  $ARTEMIS_HOME/bin/artemis create  \
+                              --silent \
+                              --no-web \
+                              --require-login \
+                              --relax-jolokia \
+                              --name artemis \
+                              --no-amqp-acceptor \
+                              --no-mqtt-acceptor \
+                              --no-stomp-acceptor \
+                              --no-hornetq-acceptor \
+                              --host "$(hostname -f)" \
+                              --data /var/artemis \
+                              --user "${ARTEMIS_USER}" \
+                              --password "${ARTEMIS_PASSWORD}" \
+                            "${ARTEMIS_INSTANCE}"
+  rm -rf /usr/artemis/tmp
+  ln -s /tmp/artemis /usr/artemis/tmp
+  cp -f /broker.xml $ARTEMIS_INSTANCE/etc
+  cp -f /jgroups-file_ping.xml $ARTEMIS_INSTANCE/etc
+fi
 
-JGROUPS_SED_EXPR="s/__CLUSTER_NAME__/${CLUSTER_NAME}/g"
-JGROUPS=/var/artemis/${CLUSTER_NAME}/etc/jgroups-file_ping.xml
-
-sed -i -e "${BROKER_SED_EXPR}" "${BROKER}"
-sed -i -e "${JGROUPS_SED_EXPR}" "${JGROUPS}"
-
+cd $ARTEMIS_INSTANCE || exit 1
 exec ./bin/artemis "$@"
