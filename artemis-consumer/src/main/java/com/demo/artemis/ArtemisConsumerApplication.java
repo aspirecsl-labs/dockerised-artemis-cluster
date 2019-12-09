7a8c0e6fbb519ee96fbfbfd9c5f8ea1d351a60bf
aspirecsl <labs@aspirecsl.com>
2019-12-09 15:41:57 +0000

    Revision Commit
    - adds a test consumer for the message queue
    - fixes an issue that prevented ports from being exposed in the artemis container

--- artemis-consumer/src/main/java/com/demo/artemis/ArtemisConsumerApplication.java
+++ artemis-consumer/src/main/java/com/demo/artemis/ArtemisConsumerApplication.java
@@ -1,14 +1,12 @@
 package com.demo.artemis;
 
-import java.util.UUID;
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 
 import javax.annotation.PreDestroy;
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.JMSException;
-import javax.jms.MessageProducer;
+import javax.jms.MessageConsumer;
 import javax.jms.Queue;
 import javax.jms.Session;
 import javax.jms.TextMessage;
@@ -21,18 +19,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
-import static java.time.LocalDateTime.now;
-
 @Configuration
 @SpringBootApplication
-public class ArtemisProducerApplication implements CommandLineRunner {
+public class ArtemisConsumerApplication implements CommandLineRunner {
 
-    private final String id;
     private final Queue queue;
     private final Connection connection;
 
-    public ArtemisProducerApplication() throws JMSException {
-        id = UUID.randomUUID().toString();
+    public ArtemisConsumerApplication() throws JMSException {
         queue = ActiveMQJMSClient.createQueue("example");
         final ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616", "artemis", "artemis");
         this.connection = connectionFactory.createConnection();
@@ -40,22 +34,21 @@ public class ArtemisProducerApplication implements CommandLineRunner {
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(ArtemisProducerApplication.class, args);
+        SpringApplication.run(ArtemisConsumerApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
         final Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
         connection.start();
-        final MessageProducer producer = session.createProducer(queue);
+        final MessageConsumer consumer = session.createConsumer(queue);
         while (true) {
-            final int groupId = ThreadLocalRandom.current().nextInt(5);
-            final TextMessage message =
-                    session.createTextMessage(
-                            String.format("[%s] - message with group id [%s] generated at: [%s]", id, groupId, now()));
-            message.setStringProperty("JMSXGroupID", "Group-" + groupId);
-            producer.send(message);
-            System.out.println("Sent message: " + message.getText() + " to node 0");
+            final TextMessage message = (TextMessage) consumer.receive(5000);
+            if (message != null) {
+                System.out.println(String.format("Message received: [%s]", message.getText()));
+                message.acknowledge();
+            }
+            TimeUnit.SECONDS.sleep(1);
         }
     }
 
